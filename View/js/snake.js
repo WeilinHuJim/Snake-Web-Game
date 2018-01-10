762972982ead26404fed11a09db5a9a2747f2e40
ha98ha <jimhuweilin@gmail.com>
2018-01-10 17:31:21 -0500

    Connect canvas with keyboard event created a start point of snake, by direction keys can move the snake point, and draw it on html

--- View/js/snake.js
+++ View/js/snake.js
@@ -2,28 +2,31 @@ $(document).ready(function(){
 
 var snakeCanvas = $("#snakeCanvas")[0];
 var ctx = snakeCanvas.getContext("2d");
-ctx.moveTo(0,0);
-ctx.lineTo(400,100);
-ctx.stroke();
+var unit = 10; // the unit size of snake and food
+var snake ={x:10,y:10};
 
-
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(snake.x*unit,snake.y*unit,unit,unit);
 	// function interact with keyboard left, up, right, down
 	$(document).keydown(function(event){
 		var key = event.which;
 		// keyCode 37 - Left, 38 - Up, 39 - Right, 40 - Down
 		if (key == 37){
-			//$("div").animate({left: '50px'});
+			snake.x--;
 		}
 		if (key == 38){
-			//$("div").animate({top: '50px'});
+			snake.y--;
 		}
 		if (key == 39){
-			//$("div").animate({left: '100px'});
+			snake.x++; 
 		}
 		if (key == 40){
-			//$("div").animate({top: '100px'});
+			snake.y++;
 		}
 
+
+		ctx.fillStyle = "#FF0000";
+		ctx.fillRect(snake.x*unit,snake.y*unit,unit,unit);
 	});
 
 });
