c0371d7969d99e659dd173e291ce5c76032eef0f
ha98ha <jimhuweilin@gmail.com>
2018-01-10 16:46:19 -0500

    Use HTML canvas to create the web game Add simple canvas code in javascript to draw a line

--- View/js/snake.js
+++ View/js/snake.js
@@ -1,21 +1,27 @@
 $(document).ready(function(){
 
+var snakeCanvas = $("#snakeCanvas")[0];
+var ctx = snakeCanvas.getContext("2d");
+ctx.moveTo(0,0);
+ctx.lineTo(400,100);
+ctx.stroke();
+
 
 	// function interact with keyboard left, up, right, down
 	$(document).keydown(function(event){
 		var key = event.which;
 		// keyCode 37 - Left, 38 - Up, 39 - Right, 40 - Down
 		if (key == 37){
-			$("div").animate({left: '50px'});
+			//$("div").animate({left: '50px'});
 		}
 		if (key == 38){
-			$("div").animate({top: '50px'});
+			//$("div").animate({top: '50px'});
 		}
 		if (key == 39){
-			$("div").animate({left: '100px'});
+			//$("div").animate({left: '100px'});
 		}
 		if (key == 40){
-			$("div").animate({top: '100px'});
+			//$("div").animate({top: '100px'});
 		}
 
 	});
