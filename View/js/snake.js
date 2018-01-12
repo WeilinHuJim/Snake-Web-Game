525a64957efd2b9461b5a42d0f5c2945a292c852
ha98ha <jimhuweilin@gmail.com>
2018-01-12 17:46:18 -0500

    Make snake an array and prepare for the growth of the snake

--- View/js/snake.js
+++ View/js/snake.js
@@ -3,12 +3,17 @@ $(document).ready(function(){
 var snakeCanvas = $("#snakeCanvas")[0];
 var ctx = snakeCanvas.getContext("2d");
 var unit = 10; // the unit size of snake and food
-var snake ={x:10,y:10};
+var direction = "Right";
+var initialsnake ={x:10,y:10};
+var snake = [initialsnake];
 var food = {x:20,y:20};
 var score = 0
 //draw food 
 draw(food.x,food.y,"blue"); //draw food square
-draw(snake.x,snake.y,"red"); //draw snake square
+for (s in snake){
+	draw(snake[s].x, snake[s].y, "red"); //draw snake square
+}
+
 
 	// empty the space, clear the trace of the snake
 	function emptySpace(){
@@ -27,24 +32,30 @@ draw(snake.x,snake.y,"red"); //draw snake square
 		var key = event.which;
 		// keyCode 37 - Left, 38 - Up, 39 - Right, 40 - Down
 		if (key == 37){
-			snake.x--;
+			snake[0].x--;
+			direction="Left";
 		}
 		if (key == 38){
-			snake.y--;
+			snake[0].y--;
+			direction="Up";
 		}
 		if (key == 39){
-			snake.x++; 
+			snake[0].x++;
+			direction="Right"; 
 		}
 		if (key == 40){
-			snake.y++;
+			snake[0].y++;
+			direction="Down";
 		}
 
-		if(food.x == snake.x && food.y == snake.y){
+		if(food.x == snake[0].x && food.y == snake[0].y){
 			score++;
 			$("#score").text(score);
 		}
 		emptySpace();
-		draw(snake.x,snake.y,"red");
+		for (s in snake){
+			draw(snake[s].x, snake[s].y, "red"); //draw snake square
+		}		
 		draw(food.x,food.y,"blue");
 		//ctx.fillStyle = "#FF0000";
 		//ctx.fillRect(snake.x*unit,snake.y*unit,unit,unit);
