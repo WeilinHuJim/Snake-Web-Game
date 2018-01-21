6c5204f0534fd00d75847a03020bd434c8b22342
ha98ha <jimhuweilin@gmail.com>
2018-01-21 18:48:48 -0500

    Encounter a problem, it is unclear how to return the value from database In current stage I can get the value from database, but when I print the value, it shows that the variable is undefined I fail the callback function either, need more time to think about it

--- database.js
+++ database.js
@@ -1,3 +1,5 @@
+//module.exports={
+
 var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost',
@@ -6,6 +8,7 @@ var connection = mysql.createConnection({
 	database: 'Snake_High_Score'
 });
 
+/*
 connection.connect(function(err){
 	if (err) throw err;
 	connection.query('CREATE TABLE snake_record(name VARCHAR(255), score INT)', function(err, result){
@@ -19,4 +22,18 @@ connection.connect(function(err){
 		});
 	});
 });
+*/
 
+	//findRecord = function(){
+		var searchResult;
+		connection.query('SELECT * FROM snake_record', function(err, results){
+			if (err) throw err;
+			console.log("original");
+    		console.log(results);
+    		searchResult = results;
+    		console.log(searchResult);
+		});
+		console.log(searchResult);
+	//}
+
+//};
