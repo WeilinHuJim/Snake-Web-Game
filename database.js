7969dbd2f7d95b336f3857cd7093a598da53c44b
ha98ha <jimhuweilin@gmail.com>
2018-01-23 23:48:46 -0500

    By using promise and async/await to achieve synchronization of database query

--- database.js
+++ database.js
@@ -1,41 +1,35 @@
-//module.exports={
-
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'your_user',
-	password: 'some_secret',
-	database: 'Snake_High_Score'
+var mysql = require('mysql');  				// get the lib of mysql
+var connection = mysql.createConnection({	// create connection with your sql
+host: 'localhost',
+user: 'your_user',
+password: 'some_secret',
+database: 'Snake_High_Score'
 });
 
-/*
-connection.connect(function(err){
-	if (err) throw err;
-	connection.query('CREATE TABLE snake_record(name VARCHAR(255), score INT)', function(err, result){
-		if (err) throw err;
-		connection.query('INSERT INTO snake_record (name, score) VALUES ("test1", 10)', function(err, result){
-			if (err) throw err;
-			connection.query('SELECT * FROM snake_record', function(err, results){
-				if (err) throw err;
-        		console.log(results);
+function searching(){						// searching function to search throw your database
+	return new Promise(function (resolve, reject){
+		try{
+			connection.query('SELECT * FROM snake_record', function(err, results){		// sql query w select everty from table snake_record
+				if (err) {
+					reject(new Error('oops'));		// if error happen reject it
+				}
+				else{
+					console.log("original");
+		    		console.log(results);				
+					resolve(results) ;				// callback the result of  searching database
+				}
 			});
-		});
+		}catch(err){
+			console.log(err.message);
+		}
 	});
-});
-*/
+}
 
-	async function searchRecord(){
-		//var searchResult;
-		var searchResult = await connection.query('SELECT * FROM snake_record', function(err, results){
-			if (err) throw err;
-			else{
-				console.log("original");
-	    		console.log(results);
-				searchResult = results;
-			}
-			
-		});	
-		console.log(searchResult);
-	}
 
-searchRecord();
+async function searchDB(){							// search database
+	var result =await searching();					// use await to let program to wait searchingDB finish first
+	console.log(result);							
+}	
+		
+
+searchDB();
