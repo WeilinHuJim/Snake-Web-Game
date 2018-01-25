098c8739f827083159ff6f0a569d3347c146f84c
ha98ha <jimhuweilin@gmail.com>
2018-01-24 22:16:21 -0500

    Achieved to integrate server with database and show database result in browser It also require async/await function to let database complete query first before

--- database.js
+++ database.js
@@ -1,3 +1,4 @@
+
 var mysql = require('mysql');  				// get the lib of mysql
 var connection = mysql.createConnection({	// create connection with your sql
 host: 'localhost',
@@ -6,30 +7,32 @@ password: 'some_secret',
 database: 'Snake_High_Score'
 });
 
-function searching(){						// searching function to search throw your database
-	return new Promise(function (resolve, reject){
-		try{
-			connection.query('SELECT * FROM snake_record', function(err, results){		// sql query w select everty from table snake_record
-				if (err) {
-					reject(new Error('oops'));		// if error happen reject it
-				}
-				else{
-					console.log("original");
-		    		console.log(results);				
-					resolve(results) ;				// callback the result of  searching database
-				}
-			});
-		}catch(err){
-			console.log(err.message);
-		}
-	});
-}
+var dbmanager={
+	searching: function(){						// searching function to search throw your database
+		return new Promise(function (resolve, reject){
+			try{
+				connection.query('SELECT * FROM snake_record', function(err, results){		// sql query w select everty from table snake_record
+					if (err) {
+						reject(new Error('oops'));		// if error happen reject it
+					}
+					else{		
+						resolve(results) ;				// callback the result of  searching database
+					}
+				});
+			}catch(err){
+				console.log(err.message);
+			}
+		});
+	},
 
 
-async function searchDB(){							// search database
-	var result =await searching();					// use await to let program to wait searchingDB finish first
-	console.log(result);							
-}	
+	searchDB: async function(){							// search database
+		var result =await this.searching();					// use await to let program to wait searchingDB finish first
+		return result;							
+	}	
 		
 
-searchDB();
+};
+module.exports = dbmanager;
+
+//searchDB();
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 
 var express = require('express');
 var app = express();
+var snake_db = require('./database');
+
 app.set('view engine', 'ejs');
 
 app.use('/', express.static(__dirname+'/views'));
 
-app.get('/', function(req,res){
+app.get('/', async function(req,res){
 	//res.sendFile(__dirname+"/"+"Snake.html");
-	res.render('Snake', {test: "Try pass value to page" });
+	var result = await snake_db.searchDB();
+	res.render('Snake', {records: result });
 })
 
 
